93228f7b61ae1d7b009e18c72052aef4259d74ce
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-11 19:47:45 -0700

    Add initial board state in data file

---
+++ js/data.js
@@ -0,0 +1,12 @@
+const board = [
+    // black start
+    ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"],
+    ["pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn"],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn"],
+    ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"],
+    // white start
+];

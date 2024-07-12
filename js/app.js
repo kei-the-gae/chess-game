73d3467f0ef3fdd289d051d460294a2f74b7c055
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-12 15:31:35 -0700

    Add update board function, add the render function and add the call for update board in render

--- js/app.js
+++ js/app.js
@@ -13,10 +13,26 @@ const boardElement = document.querySelector(".board");
 
 /*-------------- Functions -------------*/
 
+const updateBoard = () => {
+    let i = 0;
+    board.forEach((row, rowIdx) => {
+        row.forEach((square, columnIdx) => {
+            if (square !== "") { squareElements[i].textContent = board[rowIdx][columnIdx]; };
+            i++;
+        });
+    });
+};
+
+const render = () => {
+    updateBoard();
+};
+
 const init = () => {
     board = startingBoard;
     render();
-}
+};
+
+init();
 
 /*----------- Event Listeners ----------*/
 

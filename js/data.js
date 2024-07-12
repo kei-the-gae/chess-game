31fbe8f0227cbd827db858675ad4311ca93493fb
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-11 20:46:22 -0700

    Add base class constructors for each piece

--- js/data.js
+++ js/data.js
@@ -10,3 +10,60 @@ const board = [
     ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"],
     // white start
 ];
+
+class Pieces {
+    constructor() {
+
+    }
+    validDestinations() { }
+    isKingInCheck() { }
+};
+
+class King extends Pieces {
+    constructor() {
+
+    }
+    validDestinations() { }
+
+};
+
+class Queen extends Pieces {
+    constructor() {
+
+    }
+    validDestinations() { }
+
+};
+
+class Bishop extends Pieces {
+    constructor() {
+
+    }
+    validDestinations() { }
+
+};
+
+class Knight extends Pieces {
+    constructor() {
+
+    }
+    validDestinations() { }
+
+};
+
+class Rook extends Pieces {
+    constructor() {
+
+    }
+    validDestinations() { }
+
+};
+
+class Pawn extends Pieces {
+    constructor() {
+
+    }
+    validDestinations() { }
+    isRookEligibleForPromotion() { }
+
+};

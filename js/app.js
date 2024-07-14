ff8c204175fb150f033247791e3ae1647683d42f
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-14 16:19:46 -0700

    Add functionality for initializeBoard function to create objects for each player's pieces and add them to the corresponding player pieces array

--- js/app.js
+++ js/app.js
@@ -20,18 +20,27 @@ const initializeBoard = () => {
             if (square !== "") {
                 const pieceElement = document.createElement("div");
                 let pieceAttributes;
+                let pieceType;
                 const getPieceAttributes = () => {
                     let color;
                     const piece = board[rowIdx][columnIdx]
                     if (rowIdx <= 1) color = "black";
                     if (rowIdx >= 6) color = "white";
                     pieceAttributes = { color: color, piece: piece };
+                    if (piece.startsWith("R")) pieceType = Rook;
+                    if (piece.startsWith("N")) pieceType = Knight;
+                    if (piece.startsWith("B")) pieceType = Bishop;
+                    if (piece.startsWith("Q")) pieceType = Queen;
+                    if (piece.startsWith("K")) pieceType = King;
+                    if (piece.startsWith("P")) pieceType = Pawn;
                 }
                 getPieceAttributes();
 
                 pieceElement.setAttribute("class", `${pieceAttributes.color} ${pieceAttributes.piece}`);
                 pieceElement.textContent = pieceAttributes.piece;
                 squareElements[i].appendChild(pieceElement);
+                if (pieceAttributes.color === "white") whitePieces.push(new pieceType(pieceAttributes.piece, squareElements[i].getAttribute("id")));
+                if (pieceAttributes.color === "black") blackPieces.push(new pieceType(pieceAttributes.piece, squareElements[i].getAttribute("id")));
             };
             i++;
         });
@@ -80,3 +89,5 @@ boardElement.addEventListener("click", handleClick);
 
 // console.dir(squareElements);
 // console.dir(boardElement);
+console.log(whitePieces);
+console.log(blackPieces);
